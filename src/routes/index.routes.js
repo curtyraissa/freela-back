3e215cd3b9eec169823edd580ccdae247d458e0a
curtyraissa <raissacurty@gmail.com>
2023-05-24 00:11:36 -0300

    criacao de rotas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import {inserirCidades, inserirPassagens, inserirHospedagens, listarHospedagens, listarPassagens, detalhesHospedagem, detalhesPassagem} from "../controllers/controllers.js";
+import {inserirCidade, inserirPassagem, inserirHospedagem, listarHospedagens, listarPassagens, detalhesHospedagem, detalhesPassagem} from "../controllers/controllers.js";
 
 
 const freelaRouter = Router()
 
-freelaRouter.post("/cidades", inserirCidades)
-freelaRouter.post("/passagens", inserirPassagens)
-freelaRouter.post("/hospedagens", inserirHospedagens)
+freelaRouter.post("/cidades", inserirCidade)
+freelaRouter.post("/passagens", inserirPassagem)
+freelaRouter.post("/hospedagens", inserirHospedagem)
 freelaRouter.get("/passagens/:cidade_id", listarPassagens)
 freelaRouter.get("/hospedagens/:cidade_id", listarHospedagens)
 freelaRouter.get("/passagens/:cidade_id/:passagem_id", detalhesPassagem)
