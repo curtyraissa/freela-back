4092cfc060f75a93034ef6a03f5d37c922f2b573
curtyraissa <raissacurty@gmail.com>
2023-05-25 15:45:40 -0300

    correcoes

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -17,7 +17,7 @@ export async function inserirPassagem(req, res) {
     const {cidade_id, preco, cia_aerea, data, horario_saida, horario_chegada, cidade_origem, cidade_destino}= req.body
   
     try{
-      await db.query(`INSERT INTO cidades (cidade_id, preco, cia_aerea, data, horario_saida, horario_chegada, cidade_origem, cidade_destino) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,[cidade_id, preco, cia_aerea, data, horario_saida, horario_chegada, cidade_origem, cidade_destino]);
+      await db.query(`INSERT INTO passagens (cidade_id, preco, cia_aerea, data, horario_saida, horario_chegada, cidade_origem, cidade_destino) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,[cidade_id, preco, cia_aerea, data, horario_saida, horario_chegada, cidade_origem, cidade_destino]);
   
     return res.sendStatus(201)
     } catch (err) {
@@ -29,7 +29,7 @@ export async function inserirHospedagem(req, res) {
     const {cidade_id, preco_diaria, hotel, foto, cafe_da_manha, estacionamento}= req.body
   
     try{
-      await db.query(`INSERT INTO cidades (cidade_id, preco_diaria, hotel, foto, cafe_da_manha, estacionamento) VALUES ($1, $2, $3, $4, $5, $6)`,[cidade_id, preco_diaria, hotel, foto, cafe_da_manha, estacionamento]);
+      await db.query(`INSERT INTO hospedagens (cidade_id, preco_diaria, hotel, foto, cafe_da_manha, estacionamento) VALUES ($1, $2, $3, $4, $5, $6)`,[cidade_id, preco_diaria, hotel, foto, cafe_da_manha, estacionamento]);
   
     return res.sendStatus(201)
     } catch (err) {
